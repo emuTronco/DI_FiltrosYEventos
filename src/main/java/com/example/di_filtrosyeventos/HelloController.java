f5f127131e018725fc68706fa45e775aed26a090
David Patron Fernandez <tronez98@gmail.com>
2023-01-25 19:06:48 +0100

    Initial commit

---
+++ src/main/java/com/example/di_filtrosyeventos/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.di_filtrosyeventos;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
